9d22b02fb3bcc842d32fdf5564a284e4d77fb2d4
Flominator <github.flominatorgd@gmail.com>
2026-02-22 21:21:24 +0100

    Add system for on7xeltedd-user 8.1.0 M1AJQ G610FDDS1CTE6 release-keys
    Signed-off-by: Flominator <github.flominatorgd@gmail.com>

---
+++ system/bin/install-recovery.sh
@@ -0,0 +1,46 @@
+#!/system/bin/sh
+if ! applypatch -c EMMC:/dev/block/platform/13540000.dwmmc0/by-name/RECOVERY:25786656:19e9dae06737ebc43c281d67907536c0637adbaf; then
+  applypatch EMMC:/dev/block/platform/13540000.dwmmc0/by-name/BOOT:18966816:7f8ed9eeef26c5c959e86860bb185eb05c27d180 EMMC:/dev/block/platform/13540000.dwmmc0/by-name/RECOVERY 19e9dae06737ebc43c281d67907536c0637adbaf 25786656 7f8ed9eeef26c5c959e86860bb185eb05c27d180:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || echo 454 > /cache/fota/fota.status
+else
+  log -t recovery "Recovery image already installed"
+fi
+
+if [ -e /cache/recovery/command ] ; then
+  PACKAGE_PATH=""
+  SEARCH_COMMAND="--update_package"
+  PATH_POS=16
+  if [ -e '/system/bin/grep' ] ; then
+    PACKAGE_PATH=`cat /cache/recovery/command | grep 'update_package'`
+    PACKAGE_ORG_PATH=`cat /cache/recovery/command | grep 'update_org_package'`
+    if [ "$PACKAGE_ORG_PATH" != "" ] ; then
+      PACKAGE_PATH=$PACKAGE_ORG_PATH
+      SEARCH_COMMAND="--update_org_package"
+      PATH_POS=20
+    fi
+    if [ -e /cache/recovery/saved" ] ; then
+      rm -rf /cache/recovery/saved
+    fi
+
+    if [ -e /data/.recovery/saved" ] ; then
+      rm -rf /data/.recovery/saved
+    fi
+  fi
+  if [ "$PACKAGE_PATH" != "" ] ; then
+    for PACKAGE_LINE in $PACKAGE_PATH
+    do
+      if [ ${PACKAGE_LINE:0:$PATH_POS} == $SEARCH_COMMAND ] ; then
+        break
+      fi
+    done
+    let PATH_POS+=1
+    PACKAGE_PATH=${PACKAGE_LINE:$PATH_POS}
+    if [ "$PACKAGE_PATH" != "" ] ; then
+      rm $PACKAGE_PATH
+    fi
+  fi
+  if [ ${PACKAGE_PATH:0:5} == "/data" ] ; then
+    echo $PACKAGE_PATH > /cache/fota/fota_path_command
+    chown system:system /cache/fota/fota_path_command
+  fi
+  rm /cache/recovery/command
+fi
